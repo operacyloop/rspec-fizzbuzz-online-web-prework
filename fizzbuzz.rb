eab56f499c9a15b58a49ace2725d0f24d378c001
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-05-31 13:43:18 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
- if int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 3 AND 5...
-    "FizzBuzz"
-    davalue = "FizzBuzz"
-    return davalue
-    
+ if int % 3 == 0
+    if int % 5 == 0 # if the number int is divisible by 3 AND 5...
+      "FizzBuzz"
+      davalue = "FizzBuzz"
+      return davalue
+    end
   end
   
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
+  if int % 3 == 0 and # if the number int is divisible by 3
     "Fizz" # Go fizz
     int = "Fizz"
     return int
