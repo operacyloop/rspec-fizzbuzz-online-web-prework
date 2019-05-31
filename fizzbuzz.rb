40928331efcf54ae5318a9c60eb6786b047df168
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-05-31 13:54:26 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,37 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(int)
+ if int % 3 == 0
+    
+    if int % 5 == 0 # if the number int is divisible by 3 AND 5...
+      "FizzBuzz"
+      davalue = "FizzBuzz"
+      return davalue
+    end
+    
+    if davalue != "FizzBuzz"
+      davalue = "Fizz"
+      return davalue
+  end
+  
+
+  if int % 5 && davalue != "FizzBuzz" && davalue != "Fizz"
+      davalue = "Buzz"
+      return davalue
+      
+  end
+    
+    
+  end
+  
+  if int % 5 == 0 # if the number int is divisible by 5
+    "Buzz" # Go buzz
+    davalue = "Buzz"
+    return davalue
+    
+  end
+  
+ 
+  
+end
